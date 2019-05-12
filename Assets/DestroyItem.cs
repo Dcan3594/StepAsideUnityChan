0b9661513068d1806a689ece2ea43a7c1820c9ec
D_can3594 <drumcan3594@gmail.com>
2019-05-12 17:22:22 +0900

    StepAsideUnityChanプロジェクト Lesson6の課題
    発展課題未挑戦

---
+++ Assets/DestroyItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyItem : MonoBehaviour
+{
+
+    void Update()
+    {
+
+        if (!GetComponent<Renderer>().isVisible)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour {
+	bool _enabled = false;
+	Renderer _renderer;
+	void Start() {
+		_renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (!_enabled && _renderer.isVisible)
+			_enabled = true;
+		if (_enabled && !_renderer.isVisible)
+			Destroy(gameObject);
+	}
+}
